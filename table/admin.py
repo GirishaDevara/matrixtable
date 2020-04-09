4b83e2f45f45f0091a8d0ed6b785dc6c4570476c
Girisha Devara <girisha.devara@gmail.com>
2020-04-09 23:44:39 +0530

    employee class created

--- table/admin.py
+++ table/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Item
+from . models import Item, employee
 
 # Register your models here.
 class TableAdmin(admin.ModelAdmin):
--- table/models.py
+++ table/models.py
@@ -2,10 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    def __str__(self):
-        return self.name
+
     name = models.CharField(max_length=255)
     cost = models.FloatField()
     image_url = models.CharField(max_length=2083)
     description = models.CharField(max_length=1000)
+    def __str__(self):
+        return self.name
+
+
+class employee(models.Model):
+    ename = models.CharField(max_length=255)
+    eid = models.IntegerField()
+    email=models.EmailField(max_length=254)
+    def __str__(self):
+        return self.ename
 
